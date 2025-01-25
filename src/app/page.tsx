e0c62cb9b16cbcae9c18128d1cd1e0153d57c93e
bbjbc <jobc4550@gmail.com>
2025-01-26 00:40:53 +0900

    chore: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="flex min-h-screen items-center justify-center text-3xl font-extrabold text-yellow-700">
+        boongranii.dev
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
