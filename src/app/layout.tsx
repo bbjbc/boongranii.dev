fdca6fc26018c253de0a05e7e121f380a67f3838
bbjbc <jobc4550@gmail.com>
2025-01-26 01:19:39 +0900

    chore: add globals.css into styles folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
-import './globals.css';
+import '../styles/globals.css';
 
 const pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
