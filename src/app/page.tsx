f8db551f73f59fffeff85f3d00963a7fdbe106d3
bbjbc <jobc4550@gmail.com>
2025-01-26 01:07:21 +0900

    chore: add pretendard font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div>
       <h1 className="flex min-h-screen items-center justify-center text-3xl font-extrabold text-yellow-700">
-        boongranii.dev
+        boongranii.dev 안녕하세요
       </h1>
     </div>
   );
