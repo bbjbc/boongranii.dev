16941b515a5edf9cf61a5cdde24b28c9956d9a04
bbjbc <jobc4550@gmail.com>
2025-01-28 16:42:39 +0900

    chore: initial velite setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 import createMDX from '@next/mdx';
 
+const isDev = process.argv.indexOf('dev') !== -1;
+const isBuild = process.argv.indexOf('build') !== -1;
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = '1';
+  const { build } = await import('velite');
+  await build({ watch: isDev, clean: !isDev });
+}
+
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
---
+++ velite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, s } from 'velite';
+
+export default defineConfig({
+  collections: {
+    posts: {
+      name: 'Post',
+      pattern: 'posts/**/*.md',
+      schema: s
+        .object({
+          title: s.string(),
+          slug: s.slug('posts'),
+          date: s.isodate(),
+          video: s.file().optional(),
+          subTitle: s.string(),
+          image: s.image().optional(),
+          metadata: s.metadata(),
+          code: s.mdx(),
+          content: s.markdown(),
+        })
+        .transform(data => ({ ...data, permalink: `/post/${data.slug}` })),
+    },
+  },
+});
