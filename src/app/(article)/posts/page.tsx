2a2190f24948b11c59c76dd8ad38b28dd8b12179
bbjbc <jobc4550@gmail.com>
2025-01-28 20:21:33 +0900

    feat: add metadata in posts page

--- src/app/(article)/posts/page.tsx
+++ src/app/(article)/posts/page.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from 'next';
+
 import { posts } from '~content';
 
 import Title from '@/components/title';
 import PostItem from '@/components/(article)/post-item';
+
+export const metadata: Metadata = {
+  title: '느린 생각, 긴 기록',
+  description:
+    '일상에서 마주치는 순간들과 개발하며 깨달은 것들을 천천히 기록합니다.',
+};
 
 export default function Posts() {
   return (
