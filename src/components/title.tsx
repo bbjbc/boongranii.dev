0947ef95fd8b750c2485f51e221ae2efa3a15933
bbjbc <jobc4550@gmail.com>
2025-01-29 23:58:49 +0900

    style: modify title font size, color

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 const Title = ({ children }: PropsWithChildren) => {
   return (
-    <h1 className="mb-4 text-lg font-extrabold text-gray-600 dark:text-gray-200 sm:text-xl">
+    <h1 className="mb-4 text-2xl font-extrabold text-black dark:text-white sm:text-3xl">
       {children}
     </h1>
   );
