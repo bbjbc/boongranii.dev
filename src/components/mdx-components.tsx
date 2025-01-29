e56d46160ad4dce203d7b48cd27e00d46ff6482e
bbjbc <jobc4550@gmail.com>
2025-01-30 00:13:15 +0900

    feat: enhance MDX components by forwarding props for flexibility

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -4,37 +4,54 @@ import type { MDXComponents } from 'mdx/types';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    h1: ({ children }) => (
-      <h1 className="mb-4 text-2xl font-extrabold tracking-tight text-black dark:text-white sm:text-3xl">
+    h1: ({ children, props }) => (
+      <h1
+        className="mb-4 text-2xl font-extrabold tracking-tight text-black dark:text-white sm:text-3xl"
+        {...props}
+      >
         {children}
       </h1>
     ),
-    h2: ({ children }) => (
-      <h2 className="mb-3 text-xl font-extrabold text-black dark:text-white sm:text-2xl">
+    h2: ({ children, props }) => (
+      <h2
+        className="mb-3 text-xl font-extrabold text-black dark:text-white sm:text-2xl"
+        {...props}
+      >
         {children}
       </h2>
     ),
-    h3: ({ children }) => (
-      <h3 className="mb-2 text-lg font-extrabold text-black dark:text-white sm:text-xl">
+    h3: ({ children, props }) => (
+      <h3
+        className="{...props} mb-2 text-lg font-extrabold text-black dark:text-white sm:text-xl"
+        {...props}
+      >
         {children}
       </h3>
     ),
-    p: ({ children }) => <p className="mb-4 text-base">{children}</p>,
-    a: ({ children }) => (
+    p: ({ children, props }) => (
+      <p className="{...props} mb-4 text-base" {...props}>
+        {children}
+      </p>
+    ),
+    a: ({ children, props }) => (
       <a
         className="cursor-pointer font-semibold text-blue-500 decoration-2 underline-offset-2 transition-colors hover:text-blue-600 hover:underline dark:text-blue-400 dark:hover:text-blue-300"
+        {...props}
         target="_blank"
       >
         {children}
       </a>
     ),
-    ul: ({ children }) => (
-      <ul className="mb-2 ml-4 list-inside list-disc leading-loose">
+    ul: ({ children, props }) => (
+      <ul className="mb-2 ml-4 list-inside list-disc leading-loose" {...props}>
         {children}
       </ul>
     ),
-    ol: ({ children }) => (
-      <ol className="mb-2 ml-4 list-inside list-decimal leading-loose">
+    ol: ({ children, props }) => (
+      <ol
+        className="mb-2 ml-4 list-inside list-decimal leading-loose"
+        {...props}
+      >
         {children}
       </ol>
     ),
@@ -42,10 +59,14 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       <code
         className="rounded-md bg-gray-200 px-1.5 py-0.5 dark:bg-gray-700"
         {...props}
+        {...props}
       />
     ),
-    blockquote: ({ children }) => (
-      <blockquote className="mb-4 border-l-4 border-gray-600 py-2 pl-4 italic text-black dark:border-gray-300 dark:text-white">
+    blockquote: ({ children, props }) => (
+      <blockquote
+        className="mb-4 border-l-4 border-gray-600 py-2 pl-4 italic text-black dark:border-gray-300 dark:text-white"
+        {...props}
+      >
         {children}
       </blockquote>
     ),
@@ -58,6 +79,8 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         className="mb-6 rounded-md shadow-lg dark:shadow-gray-800"
       />
     ),
-    hr: () => <hr className="my-6 border-gray-300 dark:border-gray-600" />,
+    hr: props => (
+      <hr className="my-6 border-gray-300 dark:border-gray-600" {...props} />
+    ),
   };
 }
